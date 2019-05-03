2c6752c0c3dbc66630e03f938d6fce830a4989e7
roy0001919 <roy0001919@gmail.com>
2019-05-03 15:59:09 +0800

    Update

--- raspberryTest.py
+++ raspberryTest.py
@@ -17,7 +17,7 @@ mydb2 = pymssql.connect(host='192.168.0.43',
                        database='Raspberry_pi',)
                        #charset='utf8')
 logging.basicConfig(
-                    filename='testt.txt',  filemode='a',##模式，有w和a，w就是写模式，每次都会重新写日志，覆盖之前的日志
+                    filename='log.txt',  filemode='a',##模式，有w和a，w就是写模式，每次都会重新写日志，覆盖之前的日志
                     #a是追加模式，默认如果不写的话，就是追加模式
                     format=
                     '%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s'
